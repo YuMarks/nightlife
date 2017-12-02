e134d9dbe21e0f36de87169bf4feae45f0388578
Yulia M <amy.neal@rocketmail.com>
2017-12-02 13:29:25 +0000

    Oath not working on Heroku, double checking callback

---
+++ auth/old_pass.js
@@ -0,0 +1,53 @@
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var passport = require('passport');
+var session = require('express-session');
+var GithubStrategy = require('passport-github2').Strategy;  // Needed current version with Github 3.0
+var secret = process.env.MY_SECRET;
+var clientID = process.env.GITHUB_KEY;
+var clientSecret = process.env.GITHUB_SECRET;
+var userIDNum;
+
+module.exports = function(app){
+app.use(cookieParser());
+app.use(session({
+    secret: secret,
+}));
+app.use(passport.initialize());
+app.get('/auth/github', passport.authenticate('github'));
+app.get('/auth/github/callback',
+    passport.authenticate('github', { failureRedirect: '/login'}),
+    function(req, res){
+        if(req.user.profile.id){
+         req.session.userID = req.user.profile.id;
+    }   else{
+         req.session.userID = undefined;
+    }
+        return(
+           
+        res.redirect('/')        
+        );
+    }); 
+app.use(passport.session());
+passport.use(new GithubStrategy({
+    clientID: clientID,
+    clientSecret: clientSecret,
+    //callbackURL: 'https://nightlife-ymarks.c9users.io/auth/github/callback'
+    callbackURL: 'https://hidden-bastion-35960.herokuapp.com/auth/github/callback'
+}, function(accessToken, refreshToken, profile, done){
+  done(null, {
+    accessToken: accessToken,
+    profile: profile
+  });
+}));
+passport.serializeUser(function(user, done){
+   done(null, user); 
+});
+passport.deserializeUser(function(user, done){ // get the user from database and store in req.user
+   done(null, user); 
+});
+};
+
+//module.exports.userID = function(app){
+    
+//}
--- auth/pass.js
+++ auth/pass.js
@@ -8,46 +8,62 @@ var clientID = process.env.GITHUB_KEY;
 var clientSecret = process.env.GITHUB_SECRET;
 var userIDNum;
 
+
 module.exports = function(app){
-app.use(cookieParser());
-app.use(session({
-    secret: secret,
-}));
-app.use(passport.initialize());
-app.get('/auth/github', passport.authenticate('github'));
-app.get('/auth/github/callback',
-    passport.authenticate('github', { failureRedirect: '/login'}),
-    function(req, res){
-        if(req.user.profile.id){
-         req.session.userID = req.user.profile.id;
-    }   else{
-         req.session.userID = undefined;
-    }
-        return(
-           
-        res.redirect('/')        
-        );
-    }); 
+app.use(passport.initialize()); //authentication middleware
 app.use(passport.session());
-passport.use(new GithubStrategy({
-    clientID: clientID,
-    clientSecret: clientSecret,
-    //callbackURL: 'https://nightlife-ymarks.c9users.io/auth/github/callback'
-    callbackURL: 'https://hidden-bastion-35960.herokuapp.com/auth/github/callback'
+passport.use(new GithubStrategy({ //authenticate using GitHub 
+  clientID: clientID,
+  clientSecret: clientSecret,
+  //callbackURL: 'https://nightlife-ymarks.c9users.io/auth/github/callback'
+  callbackURL: 'https://hidden-bastion-35960.herokuapp.com/auth/github/callback'
 }, function(accessToken, refreshToken, profile, done){
   done(null, {
     accessToken: accessToken,
     profile: profile
   });
 }));
-passport.serializeUser(function(user, done){
-   done(null, user); 
-});
-passport.deserializeUser(function(user, done){ // get the user from database and store in req.user
-   done(null, user); 
-});
-};
 
+passport.serializeUser(function(user, done) { //translates data into storable format
+  done(null, user);
+});
+
+passport.deserializeUser(function(user, done) { // extrancts data
+  done(null, user);
+});
+
+app.get('/logout', function(req, res){
+   req.logout();
+   req.session.userID = undefined;
+   console.log("logging out");
+   res.redirect('/');
+});
+
+app.get('/auth/github',  //starts the Git Hub login process
+passport.authenticate('github'));
+
+app.get('/auth/github/callback', // Git Hub calls this url
+  passport.authenticate('github', { failureRedirect: '/login' }), //goes to Git Hub login page if failed to authenticate
+  function(req, res) {
+     
+  if(req.session.userID){  // logout 
+      req.session.userID = undefined;
+      res.redirect('/');
+  }
+      
+      var userID = req.user.profile.id;
+      req.session.userID = userID;  
+      var lastPageVisited = req.headers.referer;
+      req.session.lastPageVisited = lastPageVisited;
+      
+    // successful login, redirect to lastPageVisited
+    if(req.session.lastPageVisited === undefined){
+        res.redirect('/');
+        }else{
+            res.redirect(req.session.lastPageVisited);
+            }
+  });
+}
 module.exports.userID = function(app){
     
 }
